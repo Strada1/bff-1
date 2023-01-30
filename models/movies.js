8b2d529ea660b533c24d8d6c5c46af6cd318c1ef
Denis Soldugeev <den.soldugeev@yande.xru>
2023-01-30 18:14:49 +0400

    feat: Added new key for MovieSchema

--- models/movies.js
+++ models/movies.js
@@ -7,6 +7,10 @@ const MovieSchema = new mongoose.Schema({ // определяем схему
   },
   year: Number,
   rating: Number,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
 });
 
 const MovieModal = mongoose.model('Movie', MovieSchema);
