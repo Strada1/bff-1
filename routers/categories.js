6371184e28b2ca9fadb1143dbe427f1b93572560
Oleg Kochiev <developer-artisan@mail.ru>
2023-02-02 17:48:05 +0300

    added delete movie and category routes

--- routers/categories.js
+++ routers/categories.js
@@ -7,23 +7,36 @@ const router = Router();
 router.get(PATHS.CATEGORIES, async (req, res) => {
   try {
     const allCategories = await Categories.find({});
-    // !req.body error handler
-    return res.status(200).json(allCategories);
+    return res.status(201).json(allCategories);
   } catch (error) {
     return res.status(500).send(error);
   }
 });
 router.post(PATHS.CATEGORIES, async (req, res) => {
   try {
+    if (!req.body) {
+      return req.status(400).send('wrong JSON');
+    }
     await Categories.create(req.body);
-    // !req.body error handler
-    return res.status(200).send('category added');
+    return res.status(201).send('category added');
   } catch (error) {
     return res.status(500).send(error);
   }
 });
 
-// router.put();
+router.put('/categories/:categoryId', async (req, res) => {
+  try {
+    if (!req.body) {
+      return res.status(400).send('wrong request body!');
+    }
+    const categoryId = req.params['categoryId'];
+    const category = req.body;
+    await Categories.findByIdAndUpdate(categoryId, category);
+    return res.status(201).send('category updated!');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 // router.delete();
 
 module.exports = router;
--- routers/movies.js
+++ routers/movies.js
@@ -7,7 +7,6 @@ const router = Router();
 router.get(PATHS.MOVIES, async (req, res) => {
   try {
     const allMovies = await Movies.find({});
-    // !req.body error handler
     return res.status(200).json(allMovies);
   } catch (error) {
     return res.status(500).send(error);
@@ -16,30 +15,38 @@ router.get(PATHS.MOVIES, async (req, res) => {
 
 router.post(PATHS.MOVIES, async (req, res) => {
   try {
+    if (!req.body) {
+      return req.status(400).send('wrong JSON');
+    }
     await Movies.create(req.body);
-    // !req.body error handler
-    return res.status(200).send('movie created');
+    return res.status(201).send('movie created');
   } catch (error) {
     return res.status(500).send(error);
   }
 });
 
-router.put('/movies/:movieId', (req, res) => {
+router.put('/movies/:movieId', async (req, res) => {
   try {
+    if (!req.body) {
+      return res.status(400).send('wrong request body!');
+    }
     const movieId = req.params['movieId'];
     const movie = req.body;
-    console.log(movie, movieId);
-    return req.status(200).send('movie changed');
+    await Movies.findByIdAndUpdate({_id: movieId}, movie);
+    return res.status(201).send('movie changed');
   } catch (error) {
     return res.status(500).send(error);
   }
 });
 
-router.delete('/movies/:movieId', (req, res) => {
+router.delete('/movies/:movieId', async (req, res) => {
   try {
+    if (!req.body) {
+      return res.status(400).send('wrong request body!');
+    }
     const movieId = req.params['movieId'];
-    console.log(movieId);
-    return res.status(200).send('movie deleted');
+    await Movies.findByIdAndDelete(movieId);
+    return res.status(201).send('movie deleted');
   } catch (error) {
     return res.status(500).send(error);
   }
