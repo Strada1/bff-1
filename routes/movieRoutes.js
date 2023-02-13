98fbbf57a1aa86e916f2ee7561f5796bd9fc0756
bukadp <thedizain@gmail.com>
2023-02-13 10:05:45 +0200

    [20] Added cache

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const NodeCache = require('node-cache');
 const { findAllMovies, createMovie, findAndDelete, findAndUpdate, findItemById } = require('../services/movieService');
 const { validateMovie } = require('../middlewares');
 const { moviePostValidatorSchema, movieDeleteValidatorSchema, movieEditValidatorSchema } = require('../validatorSchema/movie');
+const myCache = new NodeCache({ stdTTL: 3600 });
 
 router.get('/', async (req, res) => {
   const { director, year, sort, onyears } = req.query;
+  const cacheKey = `${director}:${year?.toString()}:${sort?.toString()}:${onyears?.toString()} `
   try {
-    const movies = await findAllMovies({ director, year, sort, onyears });
-    return res.status(201).send(movies);
+    let allMovies = myCache.get(cacheKey);
+    if (!allMovies) {
+      allMovies = await findAllMovies({ director, year, sort, onyears });
+      myCache.set(cacheKey, allMovies);
+    }
+    return res.status(201).send(allMovies);
   } catch (err) {
     return res.status(500).send(err);
   }
@@ -36,6 +43,7 @@ router.post('/',
         duration: req.body.duration,
         director: req.body.director
       });
+      myCache.flushAll();
       return res.status(201).send('movie created');
     } catch (err) {
       return res.status(500).send(err);
@@ -49,6 +57,7 @@ router.delete('/:movieId',
     const id = req.params.movieId;
     try {
       const movie = await findAndDelete(id);
+      myCache.flushAll();
       return res.status(201).send('movie deleted');
     } catch (err) {
       return res.status(500).send(err);
@@ -70,6 +79,7 @@ router.put('/:movieId/edit',
           director: req.body.director
         },
         { new: true });
+      myCache.flushAll();
       return res.status(201).send(movie);
     } catch (err) {
       return res.status(500).send(err);
