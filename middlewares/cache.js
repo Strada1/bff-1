b7842afe1279ad1e95258aaec8ac6280135f4cd7
Oleg Kochiev <developer-artisan@mail.ru>
2023-02-19 18:47:02 +0300

    added some changes

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -3,14 +3,9 @@ const myCache = new NodeCache({checkperiod: 0});
 
 const getCache = (req, res, next) => {
   const url = req.originalUrl;
-  if (req.method !== 'GET') {
-    myCache.flushAll();
-  }
-  if (myCache.has(url)) {
-    res.status(200).json(myCache.get(url));
-  } else {
-    next();
-  }
+  if (req.method !== 'GET') myCache.flushAll();
+  if (myCache.has(url)) res.status(200).json(myCache.get(url));
+  next();
 };
 
 module.exports = {myCache, getCache};
