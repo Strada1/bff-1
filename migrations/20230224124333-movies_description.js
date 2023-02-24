9e210c25b11feb45e8724fde288275f2460a09d9
Oleg Kochiev <developer-artisan@mail.ru>
2023-02-24 20:04:25 +0300

    added some changes

--- migrations/20230224124333-movies_description.js
+++ migrations/20230224124333-movies_description.js
@@ -1,15 +1,12 @@
 module.exports = {
   async up(db, client) {
-    return db.collection('movies').updateMany(
-      {},
+    return db.collection('movies').updateMany({}, [
       {
         $set: {
           description: '$title',
         },
-      }
-    );
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+      },
+    ]);
   },
 
   async down(db, client) {
@@ -21,7 +18,5 @@ module.exports = {
         },
       }
     );
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
   },
 };
