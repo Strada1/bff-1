1e360271947ee304edc7b3c977af0aae29934380
bukadp <thedizain@gmail.com>
2023-03-01 13:39:11 +0200

    [32] Added cloud MongoDB

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const url = process.env.MONGO_URL;
+const { ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
 
+// local DB
 const connectDb = () => {
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 };
+
+//cloud DB
+// const connectDb = () => {
+//     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+// }
 
 module.exports = connectDb;
