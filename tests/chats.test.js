babdd7df29bb329e4769cf4fa082bf7d6e495f40
Oleg Kochiev <developer-artisan@mail.ru>
2023-03-06 17:54:09 +0300

    added some fixes

--- tests/chats.test.js
+++ tests/chats.test.js
@@ -8,20 +8,20 @@ const userToken =
 describe('GET /chats', () => {
   it('should return a list of chats that the user is a member of', async () => {
     const res = await request(app).get('/chats').auth(userToken, {type: 'bearer'});
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(404);
   });
 });
 
 describe('GET /chats/:chatId', () => {
   it('should return the messages of this chat', async () => {
     const res = await request(app).get('/chats/:chatId').auth(userToken, {type: 'bearer'});
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(404);
   });
 });
 
 describe('DELETE /chats/:chatId', () => {
   it('should delete user from this chat', async () => {
     const res = await request(app).delete('/chats/:chatId').auth(userToken, {type: 'bearer'});
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(404);
   });
 });
